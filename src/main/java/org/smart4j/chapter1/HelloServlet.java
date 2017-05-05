98bc2554c5af303ed600a5a6efd73b1ab4499fc9
vennnice <vennnice@163.com>
2017-05-06 00:19:04 +0800

    firstcreatedemo

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,16 @@
+package org.smart4j.chapter1;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+@WebServlet
+public class HelloServlet extends HttpServlet {
+
+}
